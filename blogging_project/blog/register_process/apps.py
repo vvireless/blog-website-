0e27760d3a2da203fe9b57f48cb71da037f1acf8
Vivian Yuan <121834289+vvireless@users.noreply.github.com>
2024-07-23 10:07:20 -0400

    Delete blogging_project/blog directory

--- blogging_project/blog/register_process/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RegisterProcessConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'register_process'
--- blogging_project/blog/website/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-def welcome(request):
-    return HttpResponse("Hi!")
-
